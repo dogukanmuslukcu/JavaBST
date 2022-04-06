cf956baa5db72f9ce414849046e396b6e25434e3
dogukanmuslukcu <dogukanmuslukcu@gmail.com>
2022-04-06 21:57:38 +0300

    Tree interface and singnatures added

---
+++ src/ITree.java
@@ -0,0 +1,9 @@
+public interface ITree {
+    public void insertion(int data);
+    public void deletion(int data);
+    public int min();
+    public int max();
+    public void traversal_LRN();
+    public void traversal_LNR();
+    public void traversal_NLR();
+}
